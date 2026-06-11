96611346a65b57c7b317836d286cdb91f4c69825
satyabrata11x <satyabrata11x@gmail.com>
2026-06-11 15:56:19 +0530

    Add Component enum

---
+++ src/main/java/com/SoftwareArchitectiurePlayGround/SoftwareArchitecturePlayground/Component/ComponentType.java
@@ -0,0 +1,11 @@
+package com.SoftwareArchitectiurePlayGround.SoftwareArchitecturePlayground.Component;
+
+public enum ComponentType {
+    CLIENT,
+    LOAD_BALANCER,
+    API_GATEWAY,
+    SERVICE,
+    DATABASE,
+    CACHE,
+    QUEUE
+}
