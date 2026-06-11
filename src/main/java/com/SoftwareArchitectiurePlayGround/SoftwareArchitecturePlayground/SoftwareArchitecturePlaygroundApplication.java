b54f9c53f262d22c6670680876241f5e7e9baded
satyabrata11x <satyabrata11x@gmail.com>
2026-06-11 10:58:53 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/SoftwareArchitectiurePlayGround/SoftwareArchitecturePlayground/SoftwareArchitecturePlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.SoftwareArchitectiurePlayGround.SoftwareArchitecturePlayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareArchitecturePlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareArchitecturePlaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SoftwareArchitectiurePlayGround/SoftwareArchitecturePlayground/SoftwareArchitecturePlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SoftwareArchitectiurePlayGround.SoftwareArchitecturePlayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareArchitecturePlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
