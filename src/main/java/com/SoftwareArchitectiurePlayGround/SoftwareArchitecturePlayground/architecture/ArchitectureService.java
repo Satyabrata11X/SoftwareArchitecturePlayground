152e78c5546d14f161ccea20294d578d3d10acad
satyabrata11x <satyabrata11x@gmail.com>
2026-06-11 14:30:29 +0530

    Implement Architecture service methods

--- src/main/java/com/SoftwareArchitectiurePlayGround/SoftwareArchitecturePlayground/architecture/ArchitectureService.java
+++ src/main/java/com/SoftwareArchitectiurePlayGround/SoftwareArchitecturePlayground/architecture/ArchitectureService.java
@@ -1,4 +1,32 @@
 package com.SoftwareArchitectiurePlayGround.SoftwareArchitecturePlayground.architecture;
 
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ArchitectureService {
+
+    private final ArchitectureRepository architectureRepository;
+
+    public ArchitectureService(ArchitectureRepository architectureRepository) {
+        this.architectureRepository = architectureRepository;
+    }
+
+    public Architecture createArchitecture(Architecture architecture){
+        return architectureRepository.save(architecture);
+    }
+
+    public List<Architecture> getAllArchitectures() {
+        return architectureRepository.findAll();
+    }
+
+    public Architecture getArchitectureById(Long id) {
+        return architectureRepository.findById(id).orElse(null);
+    }
+
+    public void deleteArchitecture(Long id) {
+        architectureRepository.deleteById(id);
+    }
 }
