e09c2b85e5fc5489c5e875c50142ef33df4f1410
satyabrata11x <satyabrata11x@gmail.com>
2026-06-11 15:02:33 +0530

    Implement Architecture CRUD APIs

--- src/main/java/com/SoftwareArchitectiurePlayGround/SoftwareArchitecturePlayground/architecture/ArchitectureController.java
+++ src/main/java/com/SoftwareArchitectiurePlayGround/SoftwareArchitecturePlayground/architecture/ArchitectureController.java
@@ -1,4 +1,39 @@
 package com.SoftwareArchitectiurePlayGround.SoftwareArchitecturePlayground.architecture;
 
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/architectures")
+
 public class ArchitectureController {
+
+    private final ArchitectureService architectureService;
+
+    public ArchitectureController(ArchitectureService architectureService) {
+        this.architectureService = architectureService;
+    }
+
+    @PostMapping
+    public Architecture createArchitecture(@RequestBody Architecture architecture){
+        return architectureService.createArchitecture(architecture);
+    }
+
+    @GetMapping
+    public List<Architecture> getAllArchitectures() {
+        return architectureService.getAllArchitectures();
+    }
+
+    @GetMapping("/{id}")
+    public Architecture getArchitectureById(@PathVariable Long id) {
+        return architectureService.getArchitectureById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteArchitecture(@PathVariable Long id) {
+        architectureService.deleteArchitecture(id);
+    }
+
 }
