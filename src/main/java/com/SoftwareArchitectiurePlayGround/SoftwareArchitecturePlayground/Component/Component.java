f156f50d069f377ad72b250244a625fa4da21e0c
satyabrata11x <satyabrata11x@gmail.com>
2026-06-11 15:53:46 +0530

    Add Component entity

--- src/main/java/com/SoftwareArchitectiurePlayGround/SoftwareArchitecturePlayground/Component/Component.java
+++ src/main/java/com/SoftwareArchitectiurePlayGround/SoftwareArchitecturePlayground/Component/Component.java
@@ -1,4 +1,33 @@
 package com.SoftwareArchitectiurePlayGround.SoftwareArchitecturePlayground.Component;
 
+
+import com.SoftwareArchitectiurePlayGround.SoftwareArchitecturePlayground.architecture.Architecture;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.type.ComponentType;
+
+@Entity
+@Table(name = "components")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+
 public class Component {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private ComponentType type;
+
+    @ManyToOne
+    @JoinColumn(name = "architecture_id")
+    private Architecture architecture;
+
 }
