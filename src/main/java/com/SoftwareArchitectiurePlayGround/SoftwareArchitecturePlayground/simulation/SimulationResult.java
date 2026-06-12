f5963e70c31b086296e3f4ebf291005744c18b35
satyabrata11x <satyabrata11x@gmail.com>
2026-06-12 19:46:54 +0530

    Add simulationresult

--- src/main/java/com/SoftwareArchitectiurePlayGround/SoftwareArchitecturePlayground/simulation/SimulationResult.java
+++ src/main/java/com/SoftwareArchitectiurePlayGround/SoftwareArchitecturePlayground/simulation/SimulationResult.java
@@ -1,4 +1,18 @@
 package com.SoftwareArchitectiurePlayGround.SoftwareArchitecturePlayground.simulation;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class SimulationResult {
+
+    private String componentName;
+
+    private double loadPercentage;
+
+    private String status;
+
 }
