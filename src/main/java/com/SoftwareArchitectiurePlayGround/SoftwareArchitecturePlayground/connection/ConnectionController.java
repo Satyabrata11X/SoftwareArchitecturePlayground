468ce222f660a48159520e9c05d92c9808c8fabe
satyabrata11x <satyabrata11x@gmail.com>
2026-06-12 19:30:59 +0530

    Implement connection CRUD APIs

--- src/main/java/com/SoftwareArchitectiurePlayGround/SoftwareArchitecturePlayground/connection/ConnectionController.java
+++ src/main/java/com/SoftwareArchitectiurePlayGround/SoftwareArchitecturePlayground/connection/ConnectionController.java
@@ -1,4 +1,36 @@
 package com.SoftwareArchitectiurePlayGround.SoftwareArchitecturePlayground.connection;
 
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/connections")
 public class ConnectionController {
+
+    private final ConnectionService connectionService;
+
+    public ConnectionController(ConnectionService connectionService) {
+        this.connectionService = connectionService;
+    }
+
+    @PostMapping
+    public Connection createConnection(@RequestBody Connection connection) {
+        return connectionService.createConnection(connection);
+    }
+
+    @GetMapping
+    public List<Connection> getAllConnections() {
+        return connectionService.getAllConnections();
+    }
+
+    @GetMapping("/{id}")
+    public Connection getConnectionById(@PathVariable Long id) {
+        return connectionService.getConnectionById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteConnection(@PathVariable Long id) {
+        connectionService.deleteConnection(id);
+    }
 }
