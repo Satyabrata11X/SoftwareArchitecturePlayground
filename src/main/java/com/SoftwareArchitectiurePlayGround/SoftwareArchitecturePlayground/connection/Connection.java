06bad5b5816e9d75f522ca1138220accf9fa7ca6
satyabrata11x <satyabrata11x@gmail.com>
2026-06-12 19:24:30 +0530

    Add connection entity

--- src/main/java/com/SoftwareArchitectiurePlayGround/SoftwareArchitecturePlayground/connection/Connection.java
+++ src/main/java/com/SoftwareArchitectiurePlayGround/SoftwareArchitecturePlayground/connection/Connection.java
@@ -1,4 +1,28 @@
 package com.SoftwareArchitectiurePlayGround.SoftwareArchitecturePlayground.connection;
 
+import com.SoftwareArchitectiurePlayGround.SoftwareArchitecturePlayground.Component.Component;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "connections")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Connection {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "source_component_id")
+    private Component sourceComponent;
+
+    @ManyToOne
+    @JoinColumn(name = "target_component_id")
+    private Component targetComponent;
+
 }
