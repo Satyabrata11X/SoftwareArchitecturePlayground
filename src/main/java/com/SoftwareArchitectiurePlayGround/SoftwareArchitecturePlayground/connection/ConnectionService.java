ec15802a97d63298b0265eb4888c5628c9285e96
satyabrata11x <satyabrata11x@gmail.com>
2026-06-12 19:28:43 +0530

    Implement connection service

--- src/main/java/com/SoftwareArchitectiurePlayGround/SoftwareArchitecturePlayground/connection/ConnectionService.java
+++ src/main/java/com/SoftwareArchitectiurePlayGround/SoftwareArchitecturePlayground/connection/ConnectionService.java
@@ -1,4 +1,31 @@
 package com.SoftwareArchitectiurePlayGround.SoftwareArchitecturePlayground.connection;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ConnectionService {
+
+    private final ConnectionRepository connectionRepository;
+
+    public ConnectionService(ConnectionRepository connectionRepository) {
+        this.connectionRepository = connectionRepository;
+    }
+
+    public Connection createConnection(Connection connection) {
+        return connectionRepository.save(connection);
+    }
+
+    public List<Connection> getAllConnections() {
+        return connectionRepository.findAll();
+    }
+
+    public Connection getConnectionById(Long id) {
+        return connectionRepository.findById(id).orElse(null);
+    }
+
+    public void deleteConnection(Long id) {
+        connectionRepository.deleteById(id);
+    }
 }
