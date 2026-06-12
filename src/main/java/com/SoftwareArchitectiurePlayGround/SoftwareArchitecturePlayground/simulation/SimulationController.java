2018a99b5eaa3d66e0a39b6f886770e01a148680
satyabrata11x <satyabrata11x@gmail.com>
2026-06-12 19:57:51 +0530

    Implemet simulation CRUD APIs

--- src/main/java/com/SoftwareArchitectiurePlayGround/SoftwareArchitecturePlayground/simulation/SimulationController.java
+++ src/main/java/com/SoftwareArchitectiurePlayGround/SoftwareArchitecturePlayground/simulation/SimulationController.java
@@ -1,4 +1,27 @@
 package com.SoftwareArchitectiurePlayGround.SoftwareArchitecturePlayground.simulation;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/simulation")
 public class SimulationController {
+
+    private final SimulationService simulationService;
+
+    public SimulationController(SimulationService simulationService) {
+        this.simulationService = simulationService;
+    }
+
+    @GetMapping("/traffic")
+    public List<SimulationResult> simulateTraffic(
+            @RequestParam Long architectureId,
+            @RequestParam int users) {
+
+        return simulationService.simulateTraffic(architectureId, users);
+    }
 }
