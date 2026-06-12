e44582cfa9428672c7d1bd9ad54b3293c3eb46fe
satyabrata11x <satyabrata11x@gmail.com>
2026-06-12 19:06:42 +0530

    Implement component CRUD APIs

--- src/main/java/com/SoftwareArchitectiurePlayGround/SoftwareArchitecturePlayground/Component/ComponentController.java
+++ src/main/java/com/SoftwareArchitectiurePlayGround/SoftwareArchitecturePlayground/Component/ComponentController.java
@@ -1,4 +1,44 @@
 package com.SoftwareArchitectiurePlayGround.SoftwareArchitecturePlayground.Component;
 
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/components")
 public class ComponentController {
+
+    private final ComponentService componentService;
+
+    public ComponentController(ComponentService componentService) {
+        this.componentService = componentService;
+    }
+
+    @PostMapping
+    public Component createComponent(@RequestBody Component component) {
+        return componentService.createComponent(component);
+    }
+
+    @GetMapping
+    public List<Component> getAllComponents() {
+        return componentService.getAllComponent();
+    }
+
+    @GetMapping("/{id}")
+    public Component getComponentById(@PathVariable Long id) {
+        return componentService.getComponentById(id);
+    }
+
+    @GetMapping("/architecture/{architectureId}")
+    public List<Component> getComponentsByArchitectureId(
+            @PathVariable Long architectureId) {
+
+        return componentService.getComponentsByArchitectureId(architectureId);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteComponent(@PathVariable Long id) {
+        componentService.deleteComponent(id);
+    }
 }
