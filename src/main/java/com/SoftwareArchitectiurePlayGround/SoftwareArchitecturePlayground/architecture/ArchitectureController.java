8295ac55b28b8dbaf8cfd0a9b7a85e4b5ee1b33c
satyabrata11x <satyabrata11x@gmail.com>
2026-06-13 20:08:32 +0530

    Configure Swagger UI and API documentation

--- src/main/java/com/SoftwareArchitectiurePlayGround/SoftwareArchitecturePlayground/architecture/ArchitectureController.java
+++ src/main/java/com/SoftwareArchitectiurePlayGround/SoftwareArchitecturePlayground/architecture/ArchitectureController.java
@@ -1,13 +1,14 @@
 package com.SoftwareArchitectiurePlayGround.SoftwareArchitecturePlayground.architecture;
 
-
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Architecture APIs", description = "Manage software architectures")
 @RestController
 @RequestMapping("/architectures")
-
 public class ArchitectureController {
 
     private final ArchitectureService architectureService;
@@ -16,24 +17,27 @@ public class ArchitectureController {
         this.architectureService = architectureService;
     }
 
+    @Operation(summary = "Create a new architecture")
     @PostMapping
-    public Architecture createArchitecture(@RequestBody Architecture architecture){
+    public Architecture createArchitecture(@RequestBody Architecture architecture) {
         return architectureService.createArchitecture(architecture);
     }
 
+    @Operation(summary = "Get all architectures")
     @GetMapping
     public List<Architecture> getAllArchitectures() {
         return architectureService.getAllArchitectures();
     }
 
+    @Operation(summary = "Get architecture by ID")
     @GetMapping("/{id}")
     public Architecture getArchitectureById(@PathVariable Long id) {
         return architectureService.getArchitectureById(id);
     }
 
+    @Operation(summary = "Delete architecture by ID")
     @DeleteMapping("/{id}")
     public void deleteArchitecture(@PathVariable Long id) {
         architectureService.deleteArchitecture(id);
     }
-
 }
