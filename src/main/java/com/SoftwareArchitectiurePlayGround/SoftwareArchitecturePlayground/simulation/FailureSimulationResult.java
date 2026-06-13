faff57897f5ae3f42603a5d842df20ff7404751e
satyabrata11x <satyabrata11x@gmail.com>
2026-06-13 19:16:35 +0530

    Add Failure simulation result

---
+++ src/main/java/com/SoftwareArchitectiurePlayGround/SoftwareArchitecturePlayground/simulation/FailureSimulationResult.java
@@ -0,0 +1,19 @@
+package com.SoftwareArchitectiurePlayGround.SoftwareArchitecturePlayground.simulation;
+
+
+import jakarta.persistence.Entity;
+import lombok.*;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class FailureSimulationResult {
+
+    private String componentName;
+
+    private String status;
+}
